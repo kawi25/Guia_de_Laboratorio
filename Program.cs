ee694ee56f407bb69a5ec88c1f2c4edb71a2d50b
Alumnos <Alumnos@LABC2PC20>
2022-08-26 10:47:38 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Guia_de_Laboratorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
